950ae8d7af9f3cf4fc56dbaccd81d767ad88b99d
Andrew Houghton <houghtonandrew0@gmail.com>
2017-11-09 08:22:39 +1100

    Wrote a script to count the number of moves, grade, rating of climbs
    Former-commit-id: ccc11ea11b2095fb01046dab863000dd3b85843f

---
+++ count_moves.py
@@ -0,0 +1,33 @@
+import pickle
+from collections import Counter
+
+all_data = pickle.load( open('mod_data.pickle', 'rb'))
+
+# print(all_data[0])
+
+# Checking what ratings exist
+ratings=[]
+for i in all_data:
+	ratings.append(all_data[i]['UserRating'])
+
+print('Ratings')
+print(Counter(ratings))
+
+# Checking what move counts exist
+moves=[]
+for i in all_data:
+	moves.append(len(all_data[i]['Moves']))
+print('Moves')
+move_count=Counter(moves)
+print(move_count)
+
+# Checking what grades exist
+grades=[]
+for i in all_data:
+	grades.append(all_data[i]['Grade'])
+print('Grades')
+print(Counter(grades))
+
+print(sorted(move_count))
+for i in sorted(move_count):
+	print('{} climbs with {} moves'.format(move_count[i],i))
