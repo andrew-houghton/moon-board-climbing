9a330b61eda1e19e73c2f66a799b55c343845ceb
Andrew Houghton <houghtonandrew0@gmail.com>
2018-03-02 17:04:19 +1100

    Finished writing script to convert dataset to PNG format
    Former-commit-id: 724f16699289cc32dcbdd29f508cbe7ffe29f622

---
+++ new_work/util/prep_HyperGAN_dataset.py
@@ -0,0 +1,11 @@
+import load_json_climbs
+from pathlib import Path
+
+base_climbset = load_json_climbs.load_all_as_climbset()
+
+for counter, climb in enumerate(base_climbset.climbs):
+	script_parent_directory = Path().resolve().parent
+	image_folder = str(script_parent_directory)+'/data/images/'
+	climb.as_image().save(image_folder+str(counter)+'.png')
+	if counter%50==0:
+		print('Created {} images.'.format(counter))
