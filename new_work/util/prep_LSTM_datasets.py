4a056c90f4c862520a44e79578c5d73a693470aa
Andrew Houghton <houghtonandrew0@gmail.com>
2018-03-02 11:04:10 +1100

    Created files to generate LSTM strings for later data processing
    Former-commit-id: c42d2ad538ff3234ec15cf3b679726ca69a9f5eb

---
+++ new_work/util/prep_LSTM_datasets.py
@@ -0,0 +1,19 @@
+import load_json_climbs
+from pathlib import Path
+
+def create_lstm_strings():
+	base_climbset = load_json_climbs.load_all_as_climbset()
+
+	string_save_directory = str(Path().resolve().parent)+'/data/strings/'
+
+	with open(str(string_save_directory)+'allclimbs_pregrade.txt','w') as handle:
+		handle.write(base_climbset.pre_grade_string())
+
+	with open(str(string_save_directory)+'allclimbs_postgrade.txt','w') as handle:
+		handle.write(base_climbset.post_grade_string())
+
+	with open(str(string_save_directory)+'allclimbs_nograde.txt','w') as handle:
+		handle.write(base_climbset.no_grade_string())
+
+if __name__ == '__main__':
+	create_lstm_strings()
