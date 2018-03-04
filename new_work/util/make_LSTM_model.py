259857642d10c1a6cc7f9557b1f808b5895d2f8d
Andrew Houghton <houghtonandrew0@gmail.com>
2018-03-04 14:03:01 +1100

    Wrote script to handle all of the connection from my script to the LSTM
    Former-commit-id: ef93ac74671a3472ba59595028f368b4cb1518e8

--- new_work/util/make_LSTM_model.py
+++ new_work/util/make_LSTM_model.py
@@ -1,24 +1,24 @@
 import os
 from pathlib import Path
-from shutil import copyfile
-import subprocess
-
 script_parent_directory = Path().resolve().parent
 
-# Move file to input folder
-filename = 'allclimbs_nograde.txt'
-string_folder = '{}/data/strings/'.format(script_parent_directory)
-string_fullpath = string_folder + filename
+#import LSTM model
+import sys
+import_path = str(script_parent_directory) + '/networks/char-rnn-tensorflow'
+sys.path.append(import_path)
+import train
+
+# Set up command arguments
+data_filename = 'allclimbs_nograde.txt'
+data_folder = '{}/data/strings/'.format(script_parent_directory)
 network_folder = '{}/networks/char-rnn-tensorflow/'.format(script_parent_directory)
-copyfile(string_fullpath, network_folder + 'data/input.txt')
+model_filename = data_filename.replace('.txt', '.ckpt')
+model_dir = '{}/data/models/{}'.format(script_parent_directory, model_filename)
 
-# Run command to train model
-# --log_dir={network}logs --save_dir={network}save'
-command = 'python {network}train.py --data_dir={network}data'
-
-subprocess.call(command.format(network=network_folder))
-
-# Save model
-model_filename = filename.replace('.txt', '.ckpt')
-model_save_path = '{}+/data/models/{}'.format(script_parent_directory, model_filename)
-copyfile(network_folder + 'save/model.ckpt', model_filename)
+train.test(
+	data_folder,
+	data_filename,
+	model_dir,
+	model_filename,
+	network_folder
+	)
