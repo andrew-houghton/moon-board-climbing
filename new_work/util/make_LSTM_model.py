8a66967365bbe9116dd83ade3368e1323119e637
Andrew Houghton <houghtonandrew0@gmail.com>
2018-03-05 21:01:18 +1100

    Integrating LSTM model access (sampling and training) into one file
    Former-commit-id: e5b5e9c821f13ca2f09396a0338d9989cd937bb3

--- new_work/util/make_LSTM_model.py
+++ new_work/util/make_LSTM_model.py
@@ -7,6 +7,7 @@ script_parent_directory = Path().resolve().parent
 network_folder = '{}/networks/char-rnn-tensorflow/'.format(script_parent_directory)
 sys.path.append(network_folder)
 import train
+import sample
 
 def train_model(grade_mode):
 	# Check that function parameters are valid
@@ -20,3 +21,17 @@ def train_model(grade_mode):
 	train.build_model(base_save_dir)
 
 train_model('no_grade')
+
+def sample_model(grade_mode):
+	# Check that function parameters are valid
+	if not grade_mode in ['no_grade','post_grade','pre_grade']:
+		raise ValueError('Invalid grade_mode for model training. Use no_grade, post_grade or pre_grade as the grade_mode parameter.')
+
+
+	# Find directories
+	base_save_dir = '{}/data/lstm_files/{}/'.format(script_parent_directory,grade_mode)
+
+	seed_str='_'
+	sample_length=500
+	sample.get_sample(base_save_dir,sample_length,seed_str):
+
