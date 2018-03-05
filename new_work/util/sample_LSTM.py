65646620d8776ac916601f7ddba29fef8822c663
Andrew Houghton <houghtonandrew0@gmail.com>
2018-03-05 23:10:11 +1100

    Fixed bug. Split LSTM training and sampling back into 2 files. Together they access the same tensorflow global(?) variables and cause an error.
    Former-commit-id: 412dc4edaf6f7a84573fbb3a1d87ae89270d6864

---
+++ new_work/util/sample_LSTM.py
@@ -0,0 +1,28 @@
+import os
+#surpress tesnsorflow warning
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from pathlib import Path
+import sys
+
+#import LSTM model
+script_parent_directory = Path().resolve().parent
+network_folder = '{}/networks/char-rnn-tensorflow/'.format(script_parent_directory)
+sys.path.append(network_folder)
+import sample
+
+def sample_model(grade_mode):
+	# Check that function parameters are valid
+	if not grade_mode in ['no_grade','post_grade','pre_grade']:
+		raise ValueError('Invalid grade_mode for model training. Use no_grade, post_grade or pre_grade as the grade_mode parameter.')
+
+
+	# Find directories
+	base_save_dir = '{}/data/lstm_files/{}/'.format(script_parent_directory,grade_mode)
+
+	seed_str='_'
+	sample_length=500
+	return sample.get_sample(base_save_dir,sample_length,seed_str)
+
+if __name__ == '__main__':
+	grade_mode = 'post_grade'
+	sample_model(grade_mode)
--- new_work/util/train_LSTM.py
+++ new_work/util/train_LSTM.py
@@ -9,7 +9,6 @@ script_parent_directory = Path().resolve().parent
 network_folder = '{}/networks/char-rnn-tensorflow/'.format(script_parent_directory)
 sys.path.append(network_folder)
 import train
-import sample
 
 def train_model(grade_mode):
 	# Check that function parameters are valid
@@ -22,20 +21,6 @@ def train_model(grade_mode):
 	# Train the model
 	train.build_model(base_save_dir)
 
-def sample_model(grade_mode):
-	# Check that function parameters are valid
-	if not grade_mode in ['no_grade','post_grade','pre_grade']:
-		raise ValueError('Invalid grade_mode for model training. Use no_grade, post_grade or pre_grade as the grade_mode parameter.')
-
-
-	# Find directories
-	base_save_dir = '{}/data/lstm_files/{}/'.format(script_parent_directory,grade_mode)
-
-	seed_str='_'
-	sample_length=500
-	return sample.get_sample(base_save_dir,sample_length,seed_str)
-
 if __name__ == '__main__':
 	grade_mode = 'post_grade'
 	train_model(grade_mode)
-	print(sample_model(grade_mode))
