ab0ac8bfa9318689b5b1d793743e1f68af32e543
Andrew Houghton <houghtonandrew0@gmail.com>
2018-03-07 08:35:18 +1100

    Made climbset navigator a class
    Former-commit-id: 59bdd14a471daef4a3c637c76288b187f425f5ec

--- new_work/explorer/layout.py
+++ new_work/explorer/layout.py
@@ -1,15 +1,36 @@
 from PIL import ImageTk
 import tkinter as tk
 
-app_root = tk.Tk()
 
-img = ImageTk.PhotoImage(file='temp.png')
+class ClimbsetNavigator:
 
-top_label = tk.Label(app_root,text = "ClimbNum").grid(column=1,columnspan=2, padx=10,pady=10)
-top_label = tk.Button(app_root,text = "<-").grid(row=1, padx=10,pady=10)
-top_label = tk.Button(app_root,text = "->").grid(column=3,row=1, padx=10,pady=10)
-top_label = tk.Label(app_root,image = img).grid(column=1,row=1,columnspan=2, padx=10,pady=10)
-top_label = tk.Button(app_root,text = "Toggle").grid(column=0,row=2,columnspan=2, padx=10,pady=10)
-top_label = tk.Button(app_root,text = "Delete").grid(column=2,row=2,columnspan=2, padx=10,pady=10)
+    def __init__(self):
+        self.app_root = tk.Tk()
 
-app_root.mainloop()
+        self.img = ImageTk.PhotoImage(file='temp.png')
+
+        
+        top_label = tk.Label(self.app_root, text="ClimbNum").grid(
+            column=1, columnspan=2, padx=10, pady=10)
+        
+        left_button = tk.Button(self.app_root, text="<-").grid(
+            row=1, padx=10, pady=10)
+        
+        right_button = tk.Button(self.app_root, text="->").grid(
+            column=3, row=1, padx=10, pady=10)
+        
+        main_image = tk.Label(self.app_root, image=self.img).grid(
+            column=1, row=1, columnspan=2, padx=10, pady=10)
+        
+        toggle_overlay = tk.Button(self.app_root, text="Toggle").grid(
+            column=0, row=2, columnspan=2, padx=10, pady=10)
+        
+        delete_button = tk.Button(self.app_root, text="Delete").grid(
+            column=2, row=2, columnspan=2, padx=10, pady=10)
+
+    def run(self):
+        self.app_root.mainloop()
+
+
+app = ClimbsetNavigator()
+app.run()
