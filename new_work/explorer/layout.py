480acabcf8e5fbc981980e1bc24b02892dfcd95a
Andrew Houghton <houghtonandrew0@gmail.com>
2018-03-07 12:58:05 +1100

    Finishing adding button functionality to climbset navigator
    Former-commit-id: 8cc867539980e979e436870d8bbe44774fb0d024

--- new_work/explorer/layout.py
+++ new_work/explorer/layout.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from PIL import ImageTk
 from PIL import Image
 
-
 import os
 import sys
 import_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/types'
@@ -22,16 +21,20 @@ class ClimbsetNavigator:
 
     def __init__(self, climbset):
         self.app_root = tk.Tk()
+        # Create the variables for managing the display
         self.climb_num = 0
+        self.overlay_visible = True
         self.climbset = climbset
 
-        self.top_label = tk.Label(self.app_root, text="ClimbNum")
-        self.left_button = tk.Button(self.app_root, text="<-",command=self.last_image)
+        # Create the display elements
+        self.top_label = tk.Label(self.app_root, text="Image 0")
+        self.left_button = tk.Button(self.app_root, text="<-", command=self.last_image)
         self.right_button = tk.Button(self.app_root, text="->", command=self.next_image)
-        self.toggle_button = tk.Button(self.app_root, text="Toggle")
+        self.toggle_button = tk.Button(self.app_root, text="Toggle", command=self.toggle_overlay)
         self.delete_button = tk.Button(self.app_root, text="Delete")
         self.main_image = tk.Label(self.app_root)
 
+        # Manage the layout
         self.top_label.grid(column=1, columnspan=2, padx=10, pady=10)
         self.left_button.grid(row=1, padx=10, pady=10)
         self.right_button.grid(column=3, row=1, padx=10, pady=10)
@@ -43,38 +46,45 @@ class ClimbsetNavigator:
 
         self.set_image_from_index()
 
+    def toggle_overlay(self):
+        self.overlay_visible = not self.overlay_visible
+
     def set_image_from_index(self):
+        # Change the image which is displayed to match the index
         image = self.climbset.climbs[self.climb_num].as_image()
         image = format_image(image)
         image.save('current.png')
         self.img = ImageTk.PhotoImage(file='current.png')
-        self.update_img()
-
-    def update_img(self):
-        self.main_image.configure(image = self.img)
+        self.main_image.configure(image=self.img)
 
     def run(self):
+        # Show the app
         self.app_root.mainloop()
 
     def next_image(self):
+        # Move to next image
         if self.climb_num < len(self.climbset.climbs) - 1:
             self.climb_num += 1
             self.set_image_from_index()
-        self.buttons_enabled_update()
+            self.update_view_state()
 
     def last_image(self):
+        # Move to previous image
         if self.climb_num > 0:
             self.climb_num += -1
             self.set_image_from_index()
-        self.buttons_enabled_update()
+            self.update_view_state()
 
-    def buttons_enabled_update(self):
+    def update_view_state(self):
+        # Check that the title at the top and the left and right buttons are in the correct state
+        self.top_label.configure(text='Image {}'.format(self.climb_num))
+
         if self.climb_num == 0:
             self.left_button.config(state=tk.DISABLED)
         elif self.climb_num >= 1:
             self.left_button.config(state=tk.NORMAL)
 
-        if self.climb_num == len(self.climbset.climbs)-1:
+        if self.climb_num == len(self.climbset.climbs) - 1:
             self.right_button.config(state=tk.DISABLED)
         elif self.climb_num <= len(self.climbset.climbs):
             self.right_button.config(state=tk.NORMAL)
