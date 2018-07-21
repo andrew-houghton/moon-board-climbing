0c10f5a5a3b391033b7a4692b083e1416ee551dd
Andrew Houghton <houghtonandrew0@gmail.com>
2018-07-21 12:32:17 +1000

    style
    Former-commit-id: 21b3eeb9140fabe74c608afa99b9e9fb741d7baf

--- training_utils/train_LSTM.py
+++ training_utils/train_LSTM.py
@@ -1,26 +1,28 @@
 import os
-#surpress tesnsorflow warning
+# surpress tesnsorflow warning
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from pathlib import Path
 import sys
 
-#import LSTM model
+# import LSTM model
 script_parent_directory = Path().resolve().parent
-network_folder = '{}/networks/char-rnn-tensorflow/'.format(script_parent_directory)
+network_folder = '{}/models/char-rnn-tensorflow/'.format(script_parent_directory)
 sys.path.append(network_folder)
 import train
 
+
 def train_model(grade_mode):
-	# Check that function parameters are valid
-	if not grade_mode in ['no_grade','post_grade','pre_grade']:
-		raise ValueError('Invalid grade_mode for model training. Use no_grade, post_grade or pre_grade as the grade_mode parameter.')
+    # Check that function parameters are valid
+    if not grade_mode in ['no_grade', 'post_grade', 'pre_grade']:
+        raise ValueError(
+            'Invalid grade_mode for model training. Use no_grade, post_grade or pre_grade as the grade_mode parameter.')
 
-	# Find directories
-	base_save_dir = '{}/data/lstm_files/{}/'.format(script_parent_directory,grade_mode)
+    # Find directories
+    base_save_dir = '{}/data/lstm_files/{}/'.format(script_parent_directory, grade_mode)
 
-	# Train the model
-	train.build_model(base_save_dir)
+    # Train the model
+    train.build_model(base_save_dir)
 
 if __name__ == '__main__':
-	grade_mode = 'post_grade'
-	train_model(grade_mode)
+    grade_mode = 'post_grade'
+    train_model(grade_mode)
