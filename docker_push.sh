f76e7b4f128fba05005f9414d217d39dac3a54b4
Andrew Houghton <houghtonandrew0@gmail.com>
2019-05-19 21:00:14 +1000

    Change to push docker images to github

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build . -t andrew-houghton/moon
-echo "$GIT_TOKEN" | docker login -u "$GIT_USERNAME" --password-stdin
+echo "$GIT_TOKEN" | docker login docker.pkg.github.com -u "$GIT_USERNAME" --password-stdin
 docker push andrew-houghton/moon
