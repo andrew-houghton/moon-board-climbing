2af2f44f6874f49e8128575f4c41cd36591316d6
Andrew Houghton <houghtonandrew0@gmail.com>
2019-06-26 16:47:11 +1000

    Basics of hyperparameter grid search implemented

--- moon/models/keras_lstm_gen/params.py
+++ moon/models/keras_lstm_gen/params.py
@@ -3,49 +3,56 @@ from typing import Any, Tuple
 from keras.optimizers import RMSprop
 from moon.models import keras_lstm_gen
 from moon.utils.load_data import load_climbset, local_file_path
+import itertools
+from pprint import pprint
+from functools import partial
 
 
 @dataclass
-class default:
-    max_climb_length: int = 12
+class Parameters:
     epochs: int = 2
+    max_climb_length: int = 12
     num_lstm_cells: Tuple[int] = (128,)
     optimizer: Any = RMSprop(lr=0.01)
     batch_size: int = 128
 
 
 @dataclass
-class parameter_space:
-    epochs: Tuple[int] = (10,)
+class ParameterSpace:
+    epochs: Tuple[int] = (2,)
     max_climb_length: Tuple[int] = (12,)
-    num_lstm_cells: Tuple[Tuple[int]] = ([128],)
+    num_lstm_cells: Tuple[Tuple[int]] = ([128], [32])
     optimizer: Tuple[Any] = (RMSprop(lr=0.01),)
-    text_diversity: Tuple[float] = (1.0,)
+    batch_size: Tuple[int] = (128,)
+
+    def product(self):
+        return list(
+            itertools.product(
+                self.epochs,
+                self.max_climb_length,
+                self.num_lstm_cells,
+                self.optimizer,
+                self.batch_size,
+            )
+        )
+
+
+def train_model(parameters, climbset):
+    lstm = keras_lstm_gen.Model()
+    history = lstm.train(climbset, parameters)
+    return parameters, history.history
 
 
 def search():
-    climbset = load_climbset(year)
-    lstm = keras_lstm_gen.Model()
-    parameter_space = arameter_space()
+    parameter_args = ParameterSpace().product()
+    hyper_parameter_combinations = [
+        Parameters(*args) for args in parameter_args
+    ]
 
-
-def main(year):
-    num_climbs = 500
-
-    # Load climbset
-
-    # Sample generators
-    sample = lstm.sample(climbset, num_climbs, sampling_parameters)
-
-    # Save to file
-    file_data = {"original": climbset, "lstm": sample}
-
-    print(f"Saving {len(file_data)} climbsets")
-    pickle.dump(
-        file_data, open(local_file_path(__file__, f"{year}.pickle"), "wb")
-    )
+    train_func = partial(train_model, climbset=load_climbset("2016"))
+    results = list(map(train_func, hyper_parameter_combinations))
+    pprint(results)
 
 
 if __name__ == "__main__":
-    main("2016")
-    main("2017")
+    search()
